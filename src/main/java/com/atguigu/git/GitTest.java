ea3470eade045c997f80aa016b647d9f79d4d5bf
kangkangi <53204760+kangkangi@users.noreply.github.com>
2021-09-24 11:29:38 +0800

    pull commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("hot_fix test");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("pull test1");
     }
 }
