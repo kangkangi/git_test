a22924a2d087e1a0a37462c90b8e233832de1cca
zjk <1271311758@qq.com>
2021-09-24 10:40:29 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author zjk
+ * @date 2021/9/24 - 10:36
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
